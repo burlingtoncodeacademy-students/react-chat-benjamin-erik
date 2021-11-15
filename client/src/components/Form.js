04e9e6d4c009f5de0019291928105979e571322b
Erik Benepe <ebenepe@gmail.com>
2021-11-15 09:00:55 -0500

    commented Form component

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,16 +1,20 @@
 import React from "react";
 
+// component for submitting messages / author name
 function Form() {
   return (
     <div id="form-container">
+      {/* input for username */}
       <form method="post" action="/chat">
         <input
           className="user-input"
+          // name property gets used by server to pass message and author to database
           name="author"
           type="text"
           placeholder="Author"
           maxLength="100"
         />
+        {/* input for message */}
         <input
           className="user-input"
           name="message"
